2a439a00107b6f5a8ca8cc1bc19db0107ea28adc
lukxpo <lucas.r.esposito@gmail.com>
2026-03-19 09:27:35 -0300

    add command line argument for prompt

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+
 def main():
-    print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("gemini api key environment variable not found")
+    
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=args.user_prompt
+    )
+
+    if response.usage_metadata == None:
+        raise RuntimeError("failed API request")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
 
 
 if __name__ == "__main__":
