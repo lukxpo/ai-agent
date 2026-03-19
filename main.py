10c4c81bf03f787d5c86cee1fcd437ceec1d3052
lukxpo <lucas.r.esposito@gmail.com>
2026-03-19 09:44:26 -0300

    add calculator app to test ai agent

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -15,16 +17,21 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=args.user_prompt
+        contents=messages
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("failed API request")
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
     print(response.text)
 
 
