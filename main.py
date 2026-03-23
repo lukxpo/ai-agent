6e261d0e07e9c8b5b11eb18d5ee62afff3f56e2d
lukxpo <lucas.r.esposito@gmail.com>
2026-03-23 07:29:24 -0300

    add loop so agent can call functions as many times it needs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -21,42 +22,52 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+            )
         )
-    )
 
-    if response.usage_metadata == None:
-        raise RuntimeError("failed API request")
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call)
-            if not function_call_result.parts:
-                raise Exception("no parts returned")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("no function_response")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("no function_response.response")
-            
-            function_results = [function_call_result.parts[0]]
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+        if response.usage_metadata == None:
+            raise RuntimeError("failed API request")
+        
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call)
+                if not function_call_result.parts:
+                    raise Exception("no parts returned")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("no function_response")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("no function_response.response")
                 
+                function_results = [function_call_result.parts[0]]
+                messages.append(types.Content(role="user", parts=function_results))
 
-    else:
-        print(response.text)
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                    
+
+        else:
+            print(response.text)
+            sys.exit(0)
+
+    print("max number of iterations reached")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
